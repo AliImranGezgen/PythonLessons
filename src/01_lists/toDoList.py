0bae68d51074b3c944b91971a050abb9ccc1e209
Ali İmran Gezgwn <aliimrangzgn23@gmail.com>
2025-12-07 22:40:51 +0300

    Refactor task management functions to accept to-do list as an argument for improved modularity and clarity

--- src/01_lists/toDoList.py
+++ src/01_lists/toDoList.py
@@ -1,17 +1,15 @@
-def taskAdded(thingsToDo) :
-    global toDoList
-    toDoList = list(map(str, thingsToDo.split(", ")))
-    printToDoList(toDoList)
-def taskDelete(deleteTaskIndex) :
-    global toDoList
+def taskAdded(thingsToDo, toDoList):
+    toDoList[:] = [i.strip() for i in thingsToDo.split(",") if i.strip()]
+
+def taskDelete(deleteTaskIndex, toDoList) :
     toDoList.pop(deleteTaskIndex-1)
+
+def markAsDone(marsAsDoneIndex, toDoList) :
+    toDoList[marsAsDoneIndex-1] = "[+]" + toDoList[marsAsDoneIndex-1]
     printToDoList(toDoList)
-def markAsDone(marsAsDoneIndex) :
-    global toDoList
-    toDoList[index-1] = "[+]" + toDoList[index-1]
-    printToDoList(toDoList)
+
 def printToDoList(toDoList):
-    a = 1;
+    a = 1
     for i in toDoList :
         print(f"{a} - {i}")
         a = a+1
@@ -19,28 +17,27 @@ def printToDoList(toDoList):
 
 
 toDoList = []
-control = " "; continueCont = "Evet"
-while continueCont == "Evet" or continueCont == "evet":
+control = " "; continueCont = "Evet"; index = 0
+while continueCont.lower() == "evet":
     control = input("Yapmak istediginiz islemi seciniz :\n  -Gorev eklemek icin 'ekle' yaz.\n  -Gorev silmek icin 'sil' yaz.\n  -Gorevi tamamlandi olarak isaretlemek icin 'isaret' yaz.\nKomut giriniz : ")
     if control == "ekle" :
         thingsToDo = input("Yapilacaklar listenize eklenecek gorevleri virgulle (,) ayirarak yaziniz : ") 
-        taskAdded(thingsToDo)
+        taskAdded(thingsToDo, toDoList)
+        printToDoList(toDoList)
     elif control == "sil" :
         printToDoList(toDoList)
-        index = 0
         index = int(input("Silmek istediginiz gorevin numarasini giriniz :"))
-        if index >0 and index <= (len(toDoList)+1):
-            taskDelete(index)
+        if index >0 and index <= (len(toDoList)):
+            taskDelete(index, toDoList)
         else: 
             print("Lutfen gecerli bir sayi giriniz :")
     elif control == "isaret":
         printToDoList(toDoList)
-        index = 0
         index = int(input("İsaretlemek istediginiz gorevin numarasini giriniz :"))
-        if index > 0 and index <= (len(toDoList)+1):
-            markAsDone(index)
+        if index > 0 and index <= (len(toDoList)):
+            markAsDone(index, toDoList)
         else: 
             print("Lutfen gecerli bir sayi giriniz :")
     else :
         control = input("Lutfen ekle, sil veya isaret komutlarindan birini seciniz. : ")
-    continueCont = input("Listenizde islem yapmaya devam etmek istiyorsaniz 'evet/Evet', istemiyorsaniz 'hayir/Hayir' giriniz.e\nKomut giriniz : ")
+    continueCont = input("Listenizde islem yapmaya devam etmek istiyorsaniz 'evet', istemiyorsaniz 'hayir' giriniz.e\nKomut giriniz : ")
